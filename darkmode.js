d3476d7b8b317f8b8481aa232ad053898cd7fb49
Wanesia <wintmiller161@gmail.com>
2025-04-05 19:23:50 +0200

    dark mode

---
+++ darkmode.js
@@ -0,0 +1,18 @@
+let darkmode = localStorage.getItem("darkmode");
+const themeToggle = document.getElementById("theme-toggle");
+
+const enableDarkMode = () => {
+  document.body.classList.add("dark-mode");
+  localStorage.setItem("darkmode", "active");
+};
+
+const disableDarkMode = () => {
+  document.body.classList.remove("dark-mode");
+  localStorage.setItem("darkmode", null);
+};
+
+if (darkmode === "active") enableDarkMode();
+themeToggle.addEventListener("click", () => {
+  darkmode = localStorage.getItem("darkmode");
+  darkmode !== "active" ? enableDarkMode() : disableDarkMode();
+});
